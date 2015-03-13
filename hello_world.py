5e28910725c4528d1cfd1bf41cfce03778871195
michael reid <michaelreid@users.noreply.github.com>
2015-03-13 14:08:52 +0000

    First edit of /jedi view.

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,14 @@
+
+#
+#  Hello World!
+#
+#  A basic Flask app to test the syntax of Flask
+#
+#  March 2015
+#
+
+
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,5 +17,24 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World!"
 
+@app.route("/hello/<name>")
+def hello_person(name):
+    html = """
+       <h1>
+          Hello {}!
+       </h1>
+       <p>
+          Here is a picture of a kitten. Awww...
+       </p>
+       <img src="http://placekitten.com/g/200/300">
+    """
+    return html.format(name.title())
+
+
+@app.route("/jedi/<first>/<second>")
+def jedi_name(first, second):
+    return "Hello {s} {f}".format(s=second, f=first)
+
+
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=8080)
+   app.run(host="0.0.0.0", port=8085)
